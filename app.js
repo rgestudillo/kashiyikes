8bd0b8a5686e594b2e01da420479486e83ce0e0a
rgestudillo <90782009+rgestudillo@users.noreply.github.com>
2023-01-23 15:24:35 +0800

    bb
    nnnnnnnnb

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.post("/scores", (req, res) => {
 
 
 
-app.listen(80, () => {
-    console.log("Server is running on port 80");
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
 });
+
